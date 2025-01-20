4c05b4f8f01a163ec44a93fec17bc9f521798d9a
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-20 21:24:43 +0000

    minor improvements

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ from constants import *
 from player import Player
 import pygame
 
-updateable = pygame.sprite.Group()
-drawable = pygame.sprite.Group()
-Player.containers = (updateable, drawable)
-
 def main():
     pygame.init()
     clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill(000)
         
         for u in updateable:
             u.update(dt)
+
+        screen.fill('black')
 
         for d in drawable:
             d.draw(screen)
