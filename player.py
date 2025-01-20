7177ab38f46847099a13adbb397e7a8406bfd264
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-20 20:24:35 +0000

    make player sprite move forwards or backwards

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import CircleShape
 import pygame
 
@@ -19,6 +19,10 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
@@ -26,6 +30,10 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(dt * -1)
+            self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
