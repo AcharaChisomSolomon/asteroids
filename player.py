7f56021903e308af6eaa1177986570c836d3d2c6
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-22 19:02:09 +0000

    give the bullets a reasonable speed

--- player.py
+++ player.py
@@ -1,9 +1,11 @@
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN
 import pygame
 from shot import Shot
 
 class Player(CircleShape):
+    shooting_timer = 0
+
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
@@ -18,9 +20,11 @@ class Player(CircleShape):
         return [a, b, c]
     
     def shoot(self):
-        new_shot = Shot(self.position.x, self.position.y)
-        new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
-        new_shot.velocity *= PLAYER_SHOOT_SPEED
+        if Player.shooting_timer < 0:
+            Player.shooting_timer = PLAYER_SHOOT_COOLDOWN
+            new_shot = Shot(self.position.x, self.position.y)
+            new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
+            new_shot.velocity *= PLAYER_SHOOT_SPEED
     
     def draw(self, screen):
         pygame.draw.polygon(screen, 'white', self.triangle(), 2)
@@ -33,6 +37,7 @@ class Player(CircleShape):
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
     def update(self, dt):
+        Player.shooting_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
