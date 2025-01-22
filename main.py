73cca55f6749deb59c2f18d84bdde4b5595e3d0e
AcharaChisomSolomon <ca0102c@gre.ac.uk>
2025-01-22 18:23:23 +0000

    add collision detection

--- main.py
+++ main.py
@@ -31,6 +31,11 @@ def main():
         for u in updateable:
             u.update(dt)
 
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                return
+
         screen.fill('black')
 
         for d in drawable:
