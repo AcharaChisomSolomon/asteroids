bb519fb1097d0106c80b1b2dcc5833af1fc09b3a
AcharaChisomSolomon <acharachisom861@gmail.com>
2025-09-03 10:53:58 +0100

    display a player in the center of the board

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 import pygame
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
